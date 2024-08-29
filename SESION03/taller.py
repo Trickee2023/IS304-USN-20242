86ba0e2e51f81cf45816f1669b66451e70dd3464
Trickee2023 <sacaperamar@unimonserrate.edu.co>
2024-08-28 20:04:16 -0500

    Update taller.py

--- SESION03/taller.py
+++ SESION03/taller.py
@@ -3,3 +3,26 @@ Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos at
 Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
 Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
 '''
+# Tarea
+class CuentaBancaria:
+    def __init__(self, numeroCta='', nombreCliente='', saldoCta=0.0, fechaApertura=''):
+        self.__numeroCta = numeroCta
+        self.__nombreCliente = nombreCliente
+        self.__saldoCta = saldoCta
+        self.__fechaApertura = fechaApertura
+        self.__ultimoRetiro = None
+        self.__ultimaConsignacion = None
+
+     def set(self, numeroCta, nombreCliente, saldoCta, fechaApertura):
+        self.__numeroCta = numeroCta
+        self.__nombreCliente = nombreCliente
+        self.__saldoCta = saldoCta
+        self.__fechaApertura = fechaApertura
+        self.__ultimoRetiro = None
+        self.__ultimaConsignacion = None
+     
+    def set(self, numeroCta, nombreCliente, saldoCta, fechaApertura):
+        self.__numeroCta = numeroCta
+        self.__nombreCliente = nombreCliente
+        self.__saldoCta = saldoCta
+        self.__fechaApertura = fechaApertura
